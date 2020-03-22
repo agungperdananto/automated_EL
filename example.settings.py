1b6ec6861975d4173807bbe1bf83664835579d20
agung287 <agung.perdananto@gmail.com>
2020-03-22 15:14:02 +0700

    add example for setting config

---
+++ example.settings.py
@@ -0,0 +1,15 @@
+# Daftar Pertanyaan untuk forum
+FORUM_QUESTIONS = [
+]
+
+LOGIN_URL = 'https://e-learning.unpam.ac.id/login/index.php'
+USERNAME = ''
+PASSWORD = ''
+
+COURSE_URL = 'https://e-learning.unpam.ac.id/course/view.php?id={}#section-{}'
+
+# Daftar Course ID
+COURSE_IDS = []
+COURSE_SECTION = []
+
+FORUM_URL = 'https://e-learning.unpam.ac.id/mod/forum/view.php?id={}'
